01d248ba45682f328a4bfea0f1f4a2637125e002
gonuguntlan <gonuguntla.n@northeastern.edu>
2021-12-10 18:32:30 -0500

    Reports 1,2

---
+++ Reports.sql
@@ -0,0 +1,16 @@
+-- Count of orders raised at a Plant.
+
+SELECT p.name,'ORDERS_COUNT', COUNT(*)
+FROM ORDERS o
+JOIN OXYGEN_CYLINDER_PLANT p
+ON o.plant_id = p.plant_id
+GROUP BY p.name;
+
+-- Number of Available Cylinders at a Plant.
+
+SELECT p.name,'Number of Available Cylinders', SUM(d.QUANTITY) AS AVAIL_CYLINDERS
+FROM OXYGEN_CYLINDER_DETAILS d
+JOIN OXYGEN_CYLINDER_PLANT p
+ON d.plant_id = p.plant_id
+WHERE AVAILABLE_STATUS = 1
+GROUP BY p.name;
