4714096adf282b8802637490bce8b10063183034
nithin-bharadwaj-369-NEU <dornipadu.n@northeastern.edu>
2021-12-15 12:19:25 -0500

    Added changes to make sure the grant permissions script work

--- setup_scripts/grant_permissions.sql
+++ setup_scripts/grant_permissions.sql
@@ -1,18 +1,13 @@
--- Author Nithin Bharadwaj
-
+---- Author Nithin Bharadwaj
+--
 --DROP USER TEST_USER_1 CASCADE; 
-
+--
 DECLARE 
-    user_exist INT;
     user_name_to_create VARCHAR2(100);
 BEGIN
-         dbms_output.put_line('>>> Creating user using Procedure');
+        dbms_output.put_line('>>> Creating user using Procedure');
          user_name_to_create:='TEST_USER_1';
-         select count(*) into user_exist from dba_users where username=user_name_to_create;
-            dbms_output.put_line('>>> User exist status ' || user_exist);
-            IF (user_exist = 0) then
-                EXECUTE IMMEDIATE 'create user ' || user_name_to_create || ' identified by Oxygenmanagementsystem123';
-            END IF;
+        EXECUTE IMMEDIATE 'create user ' || user_name_to_create || ' identified by Oxygenmanagementsystem123';
         dbms_output.put_line('>>> Allowing user to login');
         EXECUTE IMMEDIATE 'grant connect to ' || user_name_to_create ;
         dbms_output.put_line('>>> Allowing user to create a session and do ad-hoc');
@@ -28,11 +23,9 @@ BEGIN
         dbms_output.put_line('>>> Allowing user to create directory');
         EXECUTE IMMEDIATE 'grant create any directory, drop any directory to ' || user_name_to_create;
         dbms_output.put_line('>>> Granting access to crypto package');
-        EXECUTE IMMEDIATE 'grant execute on sys.dbms_crypto ' || user_name_to_create;
-        dbms_output.put_line('>>> Granting access to crypto package');
-        EXECUTE IMMEDIATE 'grant execute on sys.dbms_crypto ' || user_name_to_create;
+        EXECUTE IMMEDIATE 'grant execute on sys.dbms_crypto to ' || user_name_to_create;
         dbms_output.put_line('>>> Granting access to Random package');
-        EXECUTE IMMEDIATE 'grant execute on sys.dbms_random ' || user_name_to_create;
+        EXECUTE IMMEDIATE 'grant execute on sys.dbms_random to ' || user_name_to_create;
         dbms_output.put_line('>>> Allowing user to insert and update account table');
         EXECUTE IMMEDIATE 'GRANT INSERT ON "ADMIN"."ACCOUNT" TO ' || user_name_to_create || ' WITH GRANT OPTION' ;
         EXECUTE IMMEDIATE 'GRANT UPDATE ON "ADMIN"."ACCOUNT" TO ' || user_name_to_create || ' WITH GRANT OPTION' ;
@@ -41,10 +34,14 @@ BEGIN
         
 EXCEPTION
         WHEN OTHERS THEN
+                dbms_output.put_line(SQLERRM);
             IF SQLCODE = -1920 THEN --"table not found" exception
                 dbms_output.put_line('*** User already exists. Please create a new user');
             END IF;
     dbms_output.put_line('>>> Executuion of procedure is Done to create a new user');
 END;
 /
+
+grant execute on sys.dbms_crypto to TEST_USER_1;
+grant execute on sys.dbms_random to TEST_USER_1;
 
